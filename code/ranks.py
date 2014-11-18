71f5aaeea5786e838d632329def52c472dcb07ad
asutera <sutera.antonio@gmail.com>
2014-11-18 13:24:02 +0100

    fix pep8 for ranks.py

--- code/ranks.py
+++ code/ranks.py
@@ -7,12 +7,12 @@ from sklearn.decomposition import PCA
 # Path to network files if they are not in the same folder
 path = ''
 
-
-
 n_nodes = 1000
 
-for dataset in ["normal-1", "normal-2", "normal-3","normal-4", "lowcc", "lowcon", "highcon","highcc","normal-3-highrate","normal-4-lownoise"
-    ]:
+for dataset in ["normal-1", "normal-2", "normal-3",
+                "normal-4", "lowcc", "lowcon",
+                "highcon", "highcc", "normal-3-highrate",
+                "normal-4-lownoise"]:
 
     name = path + 'network_' + dataset + '.txt'
     raw_graph = np.loadtxt(name, delimiter=",")
@@ -21,23 +21,24 @@ for dataset in ["normal-1", "normal-2", "normal-3","normal-4", "lowcc", "lowcon"
     data = raw_graph[:, 2]
     valid_index = data > 0
     net = coo_matrix((data[valid_index], (row[valid_index], col[valid_index])),
-                        shape=(n_nodes, n_nodes)) 
+                     shape=(n_nodes, n_nodes))
 
-    graph = net.toarray() 
+    graph = net.toarray()
 
-    _, singular_values, _  = svd(graph)
+    _, singular_values, _ = svd(graph)
     singular_values /= singular_values.sum()
     plt.plot(np.sort(singular_values)[::-1], label=dataset)
-plt.legend(loc="best", prop={'size':12}).draw_frame(False)
-plt.ylabel('Singular values',size=12)
-plt.xlabel('Components',size=12)
-plt.savefig("singular_values_all.pdf", bbox_inches = 'tight')
-
+plt.legend(loc="best", prop={'size': 12}).draw_frame(False)
+plt.ylabel('Singular values', size=12)
+plt.xlabel('Components', size=12)
+plt.savefig("singular_values_all.pdf", bbox_inches='tight')
 
 plt.close()
 
-for dataset in ["normal-1", "normal-2", "normal-3","normal-4", "lowcc", "lowcon", "highcon","highcc","normal-3-highrate","normal-4-lownoise"
-    ]:
+for dataset in ["normal-1", "normal-2", "normal-3",
+                "normal-4", "lowcc", "lowcon",
+                "highcon", "highcc", "normal-3-highrate",
+                "normal-4-lownoise"]:
 
     name = path + 'network_' + dataset + '.txt'
     raw_graph = np.loadtxt(name, delimiter=",")
@@ -46,16 +47,15 @@ for dataset in ["normal-1", "normal-2", "normal-3","normal-4", "lowcc", "lowcon"
     data = raw_graph[:, 2]
     valid_index = data > 0
     net = coo_matrix((data[valid_index], (row[valid_index], col[valid_index])),
-                        shape=(n_nodes, n_nodes)) 
+                     shape=(n_nodes, n_nodes))
 
-    graph = net.toarray() 
+    graph = net.toarray()
 
-
-    clf = PCA(whiten = True)
+    clf = PCA(whiten=True)
     clf = clf.fit(graph)
     plt.plot(np.sort(clf.explained_variance_ratio_[::])[::-1], label=dataset)
-plt.legend(loc="best", prop={'size':12}).draw_frame(False)
-plt.ylabel('Explained variance ratio',size=12)
-plt.xlabel('Components',size=12)
+plt.legend(loc="best", prop={'size': 12}).draw_frame(False)
+plt.ylabel('Explained variance ratio', size=12)
+plt.xlabel('Components', size=12)
 
-plt.savefig("explained_variance_all.pdf", bbox_inches = 'tight')
+plt.savefig("explained_variance_all.pdf", bbox_inches='tight')
